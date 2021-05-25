cfc71b290562c76f7668bc04b778345b4b96e37c
Swapnil's lab <84580251+swapnil-s-lab@users.noreply.github.com>
2021-05-25 15:19:06 +0530

    25-May-21

---
+++ Python String/TotalWords.py
@@ -0,0 +1,16 @@
+"""
+    "Swapnil's Lab" is a YouTube channel            
+    based on Programming Experiments.
+       Visit my Lab to learn more.
+"""
+# Python Strings Exercises
+# {2} Total words in String
+
+s = "Don't forget to subscribe"
+l = s.split()
+print(s,'\nwords : ', len(l) )
+
+
+'''sample output'''
+# Don't forget to subscribe 
+# words :  4
