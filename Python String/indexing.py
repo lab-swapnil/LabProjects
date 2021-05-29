f2c204c5050f5003b6333709bfe410f1105e9352
Swapnil's lab <84580251+swapnil-s-lab@users.noreply.github.com>
2021-05-29 15:28:59 +0530

    0

---
+++ Python String/indexing.py
@@ -0,0 +1,24 @@
+"""
+    "Swapnil's Lab" is a YouTube         
+    channel based on Programming 
+    Experiments.
+    Visit my Lab to learn more.
+"""
+
+# Python Strings
+# {5} indexing in Python
+
+
+string = "hello dear"
+print( string[0] )  # First element
+
+list = [11, 45, 'str', 86]
+print( list[2] )    # third element
+
+tuple = (1, 2.2,'str', 3j)
+print( tuple[ len(tuple)-1 ] )  # last element
+
+''' Sample Output '''
+# h
+# str
+# 3j
