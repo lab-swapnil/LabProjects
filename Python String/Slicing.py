f96ce6ff681ad4617246acf7dbd3ac4b26bb9a6c
Swapnil's lab <84580251+swapnil-s-lab@users.noreply.github.com>
2021-06-01 14:50:49 +0530

    01-Jun-21

---
+++ Python String/Slicing.py
@@ -0,0 +1,25 @@
+"""
+    "Swapnil's Lab" is a YouTube         
+    channel based on Programming 
+    Experiments.
+    Visit my Lab to learn more.
+"""
+
+# Python Strings
+# {6} Slicing
+
+s = 'hello dear friends'
+print( s[:] )
+print( s[::] )
+print( s[::-1] )
+print( s[0:5:1] )
+print( s[6:10:1] )
+print( s[11:len(s):1] )
+
+''' sample output '''
+# hello dear friends
+# hello dear friends
+# sdneirf raed olleh
+# hello
+# dear
+# friends
