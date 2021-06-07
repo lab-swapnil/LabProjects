aae7b4ae05fe603ff71055c49ff615a3eeeb2dce
Swapnil's lab <84580251+swapnil-s-lab@users.noreply.github.com>
2021-06-07 19:39:26 +0530

    08-Jun-21

---
+++ Python List/sum.py
@@ -0,0 +1,25 @@
+"""
+    "Swapnil's Lab" is a YouTube         
+    channel based on Programming 
+    Experiments.
+    Visit my Lab to learn more.
+"""
+
+# Python Lists
+# {7} Sum of all values
+
+
+List = [11, 12, 13, 14]
+
+# Method-1
+print( sum(List) )
+
+# Method-2
+total = 0
+for e in List:
+    total = total + e
+print( total )
+
+''' Sample Output '''
+# 50
+# 50
