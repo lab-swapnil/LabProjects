4b9570d59c15b92402873c2a20eea3001aeca7ea
Swapnil's lab <84580251+swapnil-s-lab@users.noreply.github.com>
2021-06-09 20:15:59 +0530

    10-Jun-21

---
+++ Python List/product.py
@@ -0,0 +1,19 @@
+"""
+    "Swapnil's Lab" is a YouTube         
+    channel based on Programming 
+    Experiments.
+    Visit my Lab to learn more.
+"""
+
+# Python Lists
+# {8} Product of all values
+
+
+L = [1,2,3,4,5]
+product = 1
+for e in L :
+    product = product * e
+print(product)
+
+''' Sample Output '''
+# 120
